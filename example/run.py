7645ee2a7b8b8b677c7c85fd3634f3fd516c2b0f
Drew Pearce <andrew.f.pearce@accenture.com>
2019-09-30 22:10:18 -0400

    update example

--- example/run.py
+++ example/run.py
@@ -1,9 +1,15 @@
+import sys
 import yaml
+
+if len(sys.argv) > 1:
+    input_letters = sys.argv[1]
+else:
+    input_letters = ''
 
 user = 'specs',
 lang = 'Python3'
-solution = True,
+solution = input_letters,
 time = 123
 notes = '¯\_(ツ)_/¯'
 
-print(f'{user}, {lang}, {solution}, {time}, {notes}')
+print(f'{user}, {lang}, {solution}, 100, {time}, {notes}')
