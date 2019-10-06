01fbfca1fb166b4041183b082880a3de8f0bc4b1
Beejay Morgan <beejay@simplecast.com>
2019-10-05 22:30:48 -0400

    ⏱ Small optimizations

--- node-zombeej/index.js
+++ node-zombeej/index.js
@@ -1,13 +1,10 @@
-'use strict'
-
 const readline = require('readline')
 const fs = require('fs')
 
 const LETTERS = process.argv[2].split('')
 const VALUES = require('../data/letters')
 const WORDS = '../data/dictionary.txt'
-
-// console.log(LETTERS, VALUES)
+const ALPHA = 'abcdefghijklmnopqrstuvwxyz'.split('')
 
 const START = Date.now()
 
@@ -16,46 +13,57 @@ const readInterface = readline.createInterface({
   console: false
 })
 
+const lastletter = LETTERS.reduce((agg, l) => {
+  return Math.max(agg, ALPHA.indexOf(l))
+}, 0)
+
 let bestword = ['', 0]
-// let numwords = 0
+let skipper = ''
+let found = false
 
 function checkWord (word) {
   let ck = [...LETTERS]
-  let wd = word.split('')
   let total = 0
   let w = 0
   for (w; w < word.length; w++) {
     const l = word[w]
     const i = ck.indexOf(l)
-    if (i > -1) {
-      // console.log(word, l, i, ck.length, ck)
+    if (i < 0) {
+      skipper = l
+      break
+    } else {
       ck.splice(i, 1)
       total = total + VALUES[l].score 
-    } else {
-      break
     }
   }
   
   if (word.length - w > 0) {
     return false
   }
-  // console.log(word)
   if (total > bestword[1]) {
     bestword = [word, total]
   }
 }
 
 readInterface.on('line', line => {
-  if (line.length <= LETTERS.length) {
-    // numwords ++
-    checkWord(line)
+  if (found) { return }
+  if (line[0] === skipper) { return }
+  if (ALPHA.indexOf(line[0]) > lastletter) {
+    found = true
+    printResult()
+    readInterface.close()
+    return
   }
+  if (line.length > LETTERS.length) { return }
+  checkWord(line)
 })
 
 readInterface.on('close', () => {
-  const time = Date.now() - START
-  // console.log('checked words:', numwords)
-  // console.log('found', bestword)
-  // console.log(time)
-  console.log(`zombeej, Node, ${bestword[0]}, ${bestword[1]}, ${time}, hoooo doggy`)
+  if (found) { return }
+  printResult()
 })
+
+function  printResult () {
+  const time = Date.now() - START
+  console.log(`zombeej, Node, ${bestword[0]}, ${bestword[1]}, ${time}, small gainz`)
+}
